7b1dc39ddbbb6beff4adcd9b9cf50bc6e9baa2c8
depash <depash2003@gmail.com>
2022-03-11 07:58:32 -0500

    changing how backend works

--- react-app/src/components/UserPage/index.js
+++ react-app/src/components/UserPage/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import './UserPage.css'
 
 const UserPage = () => {
+    const userData = useSelector(state => state.session.stats?.user)
+    const matchData = useSelector(state => state.session.stats?.matches)
     const dispatch = useDispatch()
-
+    const history = useHistory();
+    // window.location.reload(history.push("/"));
     return (
         <div id='userPageContainer'>
-            <h1>UserPage</h1>
+            {/* <div id='profileIcon' style={{
+                backgroundImage: `url("http://ddragon.leagueoflegends.com/cdn/12.5.1/img/profileicon/${userData.profileIconId}.png")`
+            }}>
+            </div> */}
+            <div id='banner'>
+
+            </div>
+            <div id='rankDisplayAndGamesContainer'>
+                <div id='rankDisplay'>
+
+                </div>
+                <div id='gamesContainer'>
+
+                </div>
+            </div>
         </div>
     );
 };
