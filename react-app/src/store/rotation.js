46b24539381294478e438048975dd05dad7af2f1
depash <depash2003@gmail.com>
2022-04-26 22:01:09 -0400

    finsihed with store

--- react-app/src/store/rotation.js
+++ react-app/src/store/rotation.js
@@ -1,34 +1,26 @@
 const GET_ROTATION = 'session/GET_ROTATION';
 
-export const GetMatches = (name, region) => async (dispatch) => {
+const getRotation = (stats) => ({
+    type: SET_STATS,
+    payload: stats
+});
+
+export const GettingRotation = () => async (dispatch) => {
     const response = await fetch('/api/stats/matches', {
-        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name,
-            region
-        })
+        }
     });
 
     if (response.ok) {
         const data = await response.json();
-        dispatch(setMatches(data));
-    }
-    else if (response.status < 500) {
-        const data = await response.json();
-        if (data.errors) {
-            return data.errors;
-        }
-    } else {
-        return ['An error occurred. Please try again.']
+        dispatch(getRotation(data));
     }
 }
 
 const rotationReducer = function reducer(state = initialState, action) {
     switch (action.type) {
-        case SET_STATS:
+        case GET_ROTATION:
             return action.payload
         default:
             return state;
